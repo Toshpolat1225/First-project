32758cce1d926fb94cdee303b7388d7085651b8e
Toshpolat <okmkhk@gmail.com>
2021-10-21 20:09:19 +0500

    8 finish

--- src/component/FinishedQuiz/FinishedQuiz.js
+++ src/component/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./FinishedQuiz.module.css";
 import Button from "../UI/Button/Button";
+import { Link } from "react-router-dom";
 
 const FinishedQuiz = (props) => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -32,12 +33,8 @@ const FinishedQuiz = (props) => {
           True {successCount} in {props.quiz.length}
         </p>
         <div>
-          <Button onClick={props.onRetry} type="primary">
-            Retry
-          </Button>
-          <Button onClick={props.onRetry} type="success">
-            Continue
-          </Button>
+          <Button onClick={props.onRetry} type="primary">Retry</Button>
+          <Link to="/"><Button type="success">Quiz List</Button></Link>
         </div>
       </div>
     </div>
--- src/container/QuizList/QuizList.js
+++ src/container/QuizList/QuizList.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./QuizList.module.css"
 
 export default class QuizList extends Component {
+  renderQuizes(){      // Bacekenddan testlar keladi
+    return [1,2,3].map((quiz, index)=>{
+      return(
+        <li key={index} className={classes.li}>
+          <NavLink to={"/quiz/" + quiz}>
+            Test {quiz}
+          </NavLink>
+        </li>
+      )
+    })
+  }
   render() {
     return (
-      <div>
-        <h1> Quiz List </h1>
+      <div className={classes.QuizList}>
+        <div>
+          <h1 className={classes.h1}> Quiz List </h1>
+          <ul className={classes.ul}>
+            {this.renderQuizes()}
+          </ul>
+        </div>
       </div>
     );
   }

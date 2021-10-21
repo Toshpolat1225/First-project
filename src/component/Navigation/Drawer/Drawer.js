ef91f81ecdac758de810c44c22272b1d5a149251
Toshpolat <okmkhk@gmail.com>
2021-10-21 19:08:35 +0500

    8.3

--- src/component/Navigation/Drawer/Drawer.js
+++ src/component/Navigation/Drawer/Drawer.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import Backdrop from "../../UI/Backdrop/Backdrop"
 import classes from "./Drawer.module.css";
-const links = [1, 2, 3];
+import {NavLink} from "react-router-dom"
+import Backdrop from "../../UI/Backdrop/Backdrop"
+const links = [
+  {to: "/", label: "List", exact:true},
+  {to: "/auth", label: "Auth", exact:false},
+  {to: "/quiz-creator", label: "Quiz creator", exact:false},
+];
 
 class Drawer extends Component {
+  clickHandler = () =>{
+    this.props.onClose()
+  } 
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a href={link}> Link {link} </a>
+          <NavLink to={link.to} exact={link.exact} activeClassName={classes.active} onClick={this.clickHandler}>{link.label}</NavLink>
         </li>
       );
     });

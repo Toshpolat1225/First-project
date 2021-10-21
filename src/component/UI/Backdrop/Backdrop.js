8afc3f77637ec295a9a1dd1c5b748af41e3ce183
Toshpolat <okmkhk@gmail.com>
2021-10-21 18:28:46 +0500

    8.2

--- src/component/UI/Backdrop/Backdrop.js
+++ src/component/UI/Backdrop/Backdrop.js
@@ -1,4 +1,4 @@
 import React from 'react'
-import classes from './Backdrop.css'
+import classes from './Backdrop.module.css'
 const Backdrop = props => <div className={classes.Backdrop} onClick={props.onClick} />
 export default Backdrop

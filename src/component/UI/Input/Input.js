6e6fcf4755053530fd908b0ea370148d55dbbf29
Toshpolat <okmkhk@gmail.com>
2021-10-23 20:23:14 +0500

    9.2 end

--- src/component/UI/Input/Input.js
+++ src/component/UI/Input/Input.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import classes from './Input.module.css'
-function isInvalid(){
-    
+function isInvalid({valid, touched, shouldValidate}){
+    return !valid &&  shouldValidate && touched
 }
 const Input = props => {
     const inputType = props.type || 'text'
     const addClass = [classes.Input]
     const htmlFor =`${inputType}-${Math.floor(Math.random()*11)}-${Date.now()}`
-    if(true){
+    if(isInvalid(props)){
         addClass.push(classes.invalid)
     }
     return (
         <div className={addClass.join(" ")}>
             <label htmlFor={htmlFor}>{props.label}</label>
             <input type={inputType} id={htmlFor} value={props.value} onChange={props.onChange}/>
-            <span>{props.errorMessage}</span>
+            { isInvalid(props) ?  <span>{props.errorMessage || "Error message"}</span> : null }
         </div>
     )
 }

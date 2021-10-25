e6a19e8cb9853f144c453c892b247e1ab399acb5
Toshpolat <okmkhk@gmail.com>
2021-10-25 16:39:15 +0500

    9.3

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -3,14 +3,55 @@ import classes from "./Auth.module.css"
 import Button from "../../component/UI/Button/Button"
 import Input from "../../component/UI/Input/Input"
 export default class Auth extends Component {
+  state = {
+    formControls:{
+      email:{
+        value: "",
+        type: "email",
+        label: "Email",
+        errorMessage: "Error message email",
+        valid: false,
+        touched: false,
+        validation:{
+          required: true,
+          email: true,
+        }
+      },
+      password:{
+        value: "",
+        type: "password",
+        label: "Password",
+        errorMessage: "Error message password",
+        valid: false,
+        touched: false,
+        validation:{
+          required: true,
+          minLength: 6
+        }
+      },
+    }
+  }
   loginHandler = () =>{
-  
   }
   registerHandler = () =>{
-  
   }
   submitHandler = event =>{
     event.preventDefault();
+  }
+  onChangeHandler = (event, controlName) => {
+    console.log("111")
+
+  }
+  renderInputs(){
+    const inputs = Object.keys(this.state.formControls).map((controlName, index)=>{
+      const control = this.state.formControls[controlName];
+      return(
+        <Input 
+        key={controlName + index}  type={control.type}  value={control.value}  valid={control.valid}  touched={control.touched} label={control.label}  shouldValidate={!!control.validation} errorMessage={control.errorMessage} onChange={event => this.onChangeHandler(event, controlName)}/>
+      )
+    }
+    )
+    return inputs
   }
   render() {
     return (
@@ -18,8 +59,9 @@ export default class Auth extends Component {
         <div>
           <h1>Auth</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input label="Email"/>
-            <Input label="Password" errorMessage={"Test"}/>
+            {/* <Input label="Email"/>
+            <Input label="Password" errorMessage={"Test"}/> */}
+            {this.renderInputs()}
             <Button type="success" onClick={this.loginHandler}>Log in</Button>
             <Button type="primary" onClick={this.registerHandler}>Register</Button>
           </form>
